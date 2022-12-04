148e4b25d3f9c39182f17df099d9938ec6461933
Charles Dos Santos <charles.idlsc@gmail.com>
2022-12-04 10:32:53 -0300

    Aula 1 Criando o backend

---
+++ WorldCupContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorldCupSimulator
+{
+    public class WorldCupContext : DbContext
+    {
+        public WorldCupContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("ServerConnection"));
+        }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
